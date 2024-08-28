a8bfc9c404e10285be26217d9294bcef8e0abf13
yushi <yushi0206@gmail.com>
2024-08-28 10:26:20 +0900

    SSL 検証 1

--- gunicorn_settings.py
+++ gunicorn_settings.py
@@ -1,3 +1,7 @@
 import os
 
 bind = "0.0.0.0:8000"
+
+# Replace your_domain.com with your actual domain name
+certfile = "/etc/letsencrypt/live/your_domain.com/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/your_domain.com/privkey.pem"
